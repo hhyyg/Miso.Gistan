7743656239e382368d0647d8bbb3d79675541746
hiroka.85 <hiroka.85@sigmact.com>
2017-10-01 17:53:16 +0900

    add FileExtensionProvider temlate

---
+++ GistFileExtension/FileProviderItem.swift
@@ -0,0 +1,36 @@
+//
+//  FileProviderItem.swift
+//  GistFileExtension
+//
+//  Created by Hiroka Yago on 2017/10/01.
+//  Copyright © 2017 miso. All rights reserved.
+//
+
+import FileProvider
+
+class FileProviderItem: NSObject, NSFileProviderItem {
+
+    // TODO: implement an initializer to create an item from your extension's backing model
+    // TODO: implement the accessors to return the values from your extension's backing model
+    
+    var itemIdentifier: NSFileProviderItemIdentifier {
+        return NSFileProviderItemIdentifier("")
+    }
+    
+    var parentItemIdentifier: NSFileProviderItemIdentifier {
+        return NSFileProviderItemIdentifier("")
+    }
+    
+    var capabilities: NSFileProviderItemCapabilities {
+        return .allowsAll
+    }
+    
+    var filename: String {
+        return ""
+    }
+    
+    var typeIdentifier: String {
+        return ""
+    }
+    
+}
