57e47a13688834a33a053055695046e2349371e7
hiroka.85 <hiroka.85@sigmact.com>
2017-10-01 16:55:45 +0900

    Tab Iconセット

--- Gistan/Controller/MyGistsTableViewController.swift
+++ Gistan/Controller/MyGistsTableViewController.swift
@@ -59,6 +59,7 @@ class MyGistsTableViewController: UITableViewController {
         let gistItem = gistItems[indexPath.row]
         cell.textLabel?.text = gistItem.getFirstFileName()
         cell.detailTextLabel?.text = "\(gistItem.getCreatedAtText()) \(gistItem.description)"
+        //TODO:画像読み込み最適化
         cell.imageView?.image = UIImage(data: try! Data(contentsOf: URL(string: gistItem.owner.avatarUrl)!))
 
         return cell
